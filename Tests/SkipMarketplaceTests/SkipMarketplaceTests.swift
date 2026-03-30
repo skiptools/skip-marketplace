9429b0d83b9fa3b1885c71ee94b3354b940644b6
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-03-29 21:46:08 -0400

    Expand Marketplace API

--- Tests/SkipMarketplaceTests/SkipMarketplaceTests.swift
+++ Tests/SkipMarketplaceTests/SkipMarketplaceTests.swift
@@ -17,12 +17,48 @@ final class SkipMarketplaceTests: XCTestCase {
     }
 
     func testDecodeType() throws {
-        // load the TestData.json file from the Resources folder and decode it into a struct
         let resourceURL: URL = try XCTUnwrap(Bundle.module.url(forResource: "TestData", withExtension: "json"))
         let testData = try JSONDecoder().decode(TestData.self, from: Data(contentsOf: resourceURL))
         XCTAssertEqual("SkipMarketplace", testData.testModuleName)
     }
 
+    func testInstallationSourceDescription() throws {
+        let source = Marketplace.InstallationSource.unknown
+        XCTAssertEqual(source.description, "unknown")
+        XCTAssertFalse(source.isFirstPartyAppStore)
+
+        let playStore = Marketplace.InstallationSource.googlePlayStore
+        XCTAssertTrue(playStore.isFirstPartyAppStore)
+        XCTAssertEqual(playStore.description, "googlePlayStore")
+
+        let appStore = Marketplace.InstallationSource.appleAppStore
+        XCTAssertTrue(appStore.isFirstPartyAppStore)
+        XCTAssertEqual(appStore.description, "appleAppStore")
+
+        let testFlight = Marketplace.InstallationSource.testFlight
+        XCTAssertFalse(testFlight.isFirstPartyAppStore)
+        XCTAssertEqual(testFlight.description, "testFlight")
+
+        let other = Marketplace.InstallationSource.other("com.example")
+        XCTAssertFalse(other.isFirstPartyAppStore)
+    }
+
+    func testMarketplaceSingleton() throws {
+        let marketplace = Marketplace.current
+        XCTAssertNotNil(marketplace)
+    }
+
+    func testReviewRequestDelay() throws {
+        // Test custom delay
+        var called = false
+        let customDelay = Marketplace.ReviewRequestDelay(shouldCheckReview: {
+            called = true
+            return false
+        })
+        let result = customDelay.checkReviewDelay()
+        XCTAssertTrue(called)
+        XCTAssertFalse(result)
+    }
 }
 
 struct TestData : Codable, Hashable {
